8e88ea6ebfe24dfddcae5c8f8c4e5a18f2794c92
马云鹏 <1614963288@qq.com>
2019-09-20 00:26:22 +0800

    提交代码

---
+++ TaskExecutor/src/main/java/com/utils/taskExecutor/utils/TaskRunnable.java
@@ -0,0 +1,44 @@
+package com.utils.taskExecutor.utils;
+
+import com.utils.taskExecutor.Bean.Task;
+import lombok.Data;
+import org.quartz.CronExpression;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.TriggerBuilder;
+
+import java.util.Date;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 为适应Cron表达式，使用动态生成下次执行时间的方法
+ * @author myp
+ * @since 2019-09-19 23:52:38
+ */
+@Data
+public class TaskRunnable implements Runnable {
+    private Runnable runnable;
+    private String cron;
+    private Task task;
+    private ScheduledExecutorService service;
+
+    @Override
+    public void run() {
+        runnable.run();
+        Future<?> future = service.schedule(this, this.getNextTriggerTime(), TimeUnit.MILLISECONDS);
+        task.setFuture(future);
+    }
+
+    //获取下次执行时间
+    public long getNextTriggerTime(){
+        if(!CronExpression.isValidExpression(cron)){
+            return 0;
+        }
+        CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity("Caclulate Date").withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+        Date time0 = trigger.getStartTime();
+        Date time1 = trigger.getFireTimeAfter(time0);
+        return (time1.getTime() - new Date().getTime());
+    }
+}
